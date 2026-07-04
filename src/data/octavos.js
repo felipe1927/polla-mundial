e50debb35dc71b9319baab2a68ef44ca04a3f9e2
Felipe Londoño <andres970114@gmail.com>
2026-07-03 23:41:34 -0500

    8vos

--- src/data/octavos.js
+++ src/data/octavos.js
@@ -6,10 +6,10 @@ const PARTIDOS_R16 = [
   { id: "W90", local: "Paraguay", visitante: "Francia", fecha: "04 jul 2026", hora: "2026-07-04T16:00:00", flagLocal: "py", flagVisitante: "fr" },
   { id: "W91", local: "Brasil", visitante: "Noruega", fecha: "05 jul 2026", hora: "2026-07-05T15:00:00", flagLocal: "br", flagVisitante: "no" },
   { id: "W92", local: "México", visitante: "Inglaterra", fecha: "05 jul 2026", hora: "2026-07-05T19:00:00", flagLocal: "mx", flagVisitante: "gb-eng" },
-  { id: "W93", local: "W83", visitante: "W84", fecha: "06 jul 2026", hora: "2026-07-06T14:00:00", flagLocal: "eu", flagVisitante: "eu" },
-  { id: "W94", local: "Bélgica", visitante: "W82", fecha: "06 jul 2026", hora: "2026-07-06T19:00:00", flagLocal: "be", flagVisitante: "eu" },
-  { id: "W95", local: "W86", visitante: "W88", fecha: "07 jul 2026", hora: "2026-07-07T11:00:00", flagLocal: "eu", flagVisitante: "eu" },
-  { id: "W96", local: "W85", visitante: "W87", fecha: "07 jul 2026", hora: "2026-07-07T15:00:00", flagLocal: "eu", flagVisitante: "eu" },
+  { id: "W93", local: "Portugal", visitante: "España", fecha: "06 jul 2026", hora: "2026-07-06T14:00:00", flagLocal: "pt", flagVisitante: "es" },
+  { id: "W94", local: "USA", visitante: "Bélgica", fecha: "06 jul 2026", hora: "2026-07-06T19:00:00", flagLocal: "us", flagVisitante: "be" },
+  { id: "W95", local: "Argentina", visitante: "Egipto", fecha: "07 jul 2026", hora: "2026-07-07T11:00:00", flagLocal: "ar", flagVisitante: "eg" },
+  { id: "W96", local: "Suiza", visitante: "Colombia", fecha: "07 jul 2026", hora: "2026-07-07T15:00:00", flagLocal: "ch", flagVisitante: "co" },
 ]
 
 export default PARTIDOS_R16
