d3bf252f6e463cabff8af18011ca406605a12fe6
Felipe Londoño <andres970114@gmail.com>
2026-07-12 10:41:30 -0500

    semis

--- src/data/semis.js
+++ src/data/semis.js
@@ -2,8 +2,8 @@
 // Placeholder: cruces de QF
 // Se actualizarán con los cruces reales cuando termine fase de QF
 const PARTIDOS_SF = [
-  { id: "W101", local: "W97", visitante: "W98", fecha: "14 jul 2026", hora: "2026-07-14T14:00:00", flagLocal: "eu", flagVisitante: "eu" },
-  { id: "W102", local: "W99", visitante: "W100", fecha: "15 jul 2026", hora: "2026-07-15T14:00:00", flagLocal: "eu", flagVisitante: "eu" },
+  { id: "W101", local: "Francia", visitante: "España", fecha: "14 jul 2026", hora: "2026-07-14T14:00:00", flagLocal: "fr", flagVisitante: "es" },
+  { id: "W102", local: "Inglaterra", visitante: "Argentina", fecha: "15 jul 2026", hora: "2026-07-15T14:00:00", flagLocal: "gb-eng", flagVisitante: "ar" },
 ]
 
 export default PARTIDOS_SF
